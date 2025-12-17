746bef4027368c729f5ff5093dddb2ab0ddca6f5
Onkar Nardekar <79778637+OmiDevProjects@users.noreply.github.com>
2025-12-17 06:40:52 +0000

    datatype conversion

---
+++ 01_Basics/datatype_conversion.js
@@ -0,0 +1,20 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(typeof(valueInNumber));
+
+// "33" => 33
+// null => NaN
+// 33abc => NaN
+// true = 1 and false = 0
+
+// Boolean 
+// "" => false
+// "hsd" => true
+
+// string
+// 33 => "33"
